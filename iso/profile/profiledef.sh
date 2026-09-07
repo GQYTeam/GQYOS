83d60c6d6b6ee1e314359a7706c341222e0d74e0
Black Cat <203426472+Francis-Xavier-code@users.noreply.github.com>
2026-09-07 17:21:36 +0800

    fix(iso): 使用正确的 profiledef.sh 变量名 (iso_name)
    变量名错误导致 mkarchiso 报 realpath 错误:
    - profile_name → iso_name
    - profile_version → iso_version
    - profile_publisher → iso_publisher
    - profile_application_name → iso_application
    - 添加 pacman.conf, syslinux.cfg

--- iso/profile/profiledef.sh
+++ iso/profile/profiledef.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
-# GQYOS archiso profile 定义
+# shellcheck disable=SC2034
 
-profile_name="GQYOS"
-profile_version="v$(date +%Y.%m.%d)"
-profile_publisher="GQYOS Team"
-profile_application_name="GQYOS Install Medium"
-
-# 使用 grub
-bootmode=("bios-efi-x86_64-grub")
-
-user_shell="/bin/bash"
+iso_name="gqyos"
+iso_label="GQYOS_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="GQYOS Team <https://github.com/GQYTeam/GQYOS>"
+iso_application="GQYOS Install Medium"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux'
+           'uefi.systemd-boot')
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+)
