3559bb37e4dec27971f33869c331c543140d4d8f
Black Cat <203426472+Francis-Xavier-code@users.noreply.github.com>
2026-09-07 17:18:46 +0800

    fix(iso): 使用 GRUB 替代 systemd-boot，添加 grub.cfg

--- iso/profile/profiledef.sh
+++ iso/profile/profiledef.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 # GQYOS archiso profile 定义
-# 参考: https://wiki.archlinux.org/title/Archiso
 
-# ========== 基础信息 ========== #
 profile_name="GQYOS"
 profile_version="v$(date +%Y.%m.%d)"
 profile_publisher="GQYOS Team"
 profile_application_name="GQYOS Install Medium"
 
-# ========== 引导加载 ========== #
-# 使用 systemd-boot (UEFI)
-bootmode=("uefi-x64-systemd-boot")
+# 使用 grub
+bootmode=("bios-efi-x86_64-grub")
 
-# ========== 用户 ========== #
-user_shell="/bin/zsh"
+user_shell="/bin/bash"
